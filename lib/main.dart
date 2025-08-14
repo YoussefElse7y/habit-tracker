881c9ec7bdbf4db8513bf2c3e1e7e137ffe8ce4b
youssef <youssefele7y422@gmail.com>
2025-08-14 15:05:22 +0300

    refactor : main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'features/authentication/presentation/cubit/auth_cubit.dart';
-import 'features/authentication/domain/usecases/login_user.dart';
-import 'features/authentication/domain/usecases/register_user.dart';
-import 'features/authentication/domain/usecases/logout_user.dart';
-import 'features/authentication/domain/usecases/get_current_user.dart';
-import 'features/authentication/domain/usecases/send_password_reset_email.dart';
-import 'features/authentication/domain/usecases/update_profile.dart';
-import 'injection_container.dart' as di; // Your service locator
+
 
 
 void main() async {
